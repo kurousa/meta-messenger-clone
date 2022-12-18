171c9d6973b5b401904caf7458f50c40ce2f7617
kurousa <r.takahashi.1026@gmail.com>
2022-12-18 17:39:18 +0900

    fix: form bg color

--- src/app/ChatInput.tsx
+++ src/app/ChatInput.tsx
@@ -54,7 +54,7 @@ function ChatInput() {
     <form
       onSubmit={addMessage}
       className='fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2
-      border-t border-gray-100'>
+      border-t border-gray-100 bg-white'>
       <input
         type="text"
         value={input}
