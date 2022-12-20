cfe071619e6dcaee1207b71e22df711494615373
kurousa <r.takahashi.1026@gmail.com>
2022-12-20 21:00:00 +0900

    add Pusher

---
+++ src/lib/pusher.ts
@@ -0,0 +1,18 @@
+import Pusher from "pusher";
+import ClientPusher from "pusher-js";
+
+export const serverPusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.PUSHER_CLUSTER!,
+  useTLS: true
+});
+
+export const clientPusher = new ClientPusher(
+  'ef555a3c2c5d20caa94f',
+  {
+    cluster: 'ap3',
+    forceTLS: true
+  }
+);
